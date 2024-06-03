9e64221d008c62aaa38afbf5f80e289e2e8a8551
Hydraspamj <167394965+Hydraspamj@users.noreply.github.com>
2024-06-04 00:15:27 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,145 @@
+import os
+import re
+from os import getenv
+
+from dotenv import load_dotenv
+from pyrogram import filters
+
+load_dotenv()
+# Get this value from my.telegram.org/apps
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
+
+# Get your token from @BotFather on Telegram.
+BOT_TOKEN = getenv("BOT_TOKEN")
+
+# Get your mongo url from cloud.mongodb.com
+MONGO_DB_URI = getenv("MONGO_DB_URI", None)
+
+DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 16000))
+
+# Chat id of a group for logging bot's activities
+LOGGER_ID = int(getenv("LOGGER_ID"))
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1002241612045"))
+
+# Get this value from  on Telegram by /id
+OWNER_ID = int(getenv("OWNER_ID"))
+
+# Fill these variables if you're deploying on heroku.
+# Your heroku app name
+HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
+# Get it from http://dashboard.heroku.com/account
+HEROKU_API_KEY = getenv("HEROKU_API_KEY")
+
+UPSTREAM_REPO = getenv(
+    "UPSTREAM_REPO",
+    "https://github.com/Hydraspamj/Hina-assistant",
+)
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
+GIT_TOKEN = getenv(
+    "GIT_TOKEN", None
+)  # Fill this variable if your upstream repository is private
+
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/botkild")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/botkilda")
+
+# Maximum Limit Allowed for users to save playlists on bot's server
+SERVER_PLAYLIST_LIMIT = int(getenv("SERVER_PLAYLIST_LIMIT", "100"))
+
+RADIO_URL = getenv("RADIO_URL", "http://peridot.streamguys.com:7150/Mirchi")
+
+# Don't fill here any YouTube link fill here any direct acessable audio link
+
+# MaximuM limit for fetching playlist's track from youtube, spotify, apple
+# links.
+PLAYLIST_FETCH_LIMIT = int(getenv("PLAYLIST_FETCH_LIMIT", "100"))
+# Set this to True if you want the assistant to automatically leave chats
+# after an interval
+AUTO_LEAVING_ASSISTANT = False
+
+# Get this credentials from https://developer.spotify.com/dashboard
+SPOTIFY_CLIENT_ID = getenv("SPOTIFY_CLIENT_ID", "19609edb1b9f4ed7be0c8c1342039362")
+SPOTIFY_CLIENT_SECRET = getenv(
+    "SPOTIFY_CLIENT_SECRET", "409e31d3ddd64af08cfcc3b0f064fcbe"
+)
+
+
+# Maximum limit for fetching playlist's track from youtube, spotify, apple
+# links.
+PLAYLIST_FETCH_LIMIT = int(getenv("PLAYLIST_FETCH_LIMIT", 2500))
+
+
+# Telegram audio and video file size limit (in bytes)
+TG_AUDIO_FILESIZE_LIMIT = int(getenv("TG_AUDIO_FILESIZE_LIMIT", 104857600))
+TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", 1073741824))
+# Checkout https://www.gbmb.org/mb-to-bytes for converting mb to bytes
+
+# Time after which bot will suggest random chats about bot commands.
+AUTO_SUGGESTION_TIME = int(
+    getenv("AUTO_SUGGESTION_TIME", "3")
+)  # Remember to give value in Seconds
+
+# Set it True if you want to bot to suggest about bot commands to random
+# chats of your bots.
+AUTO_SUGGESTION_MODE = getenv("AUTO_SUGGESTION_MODE", "True")
+# Cleanmode time after which bot will delete its old messages from chats
+CLEANMODE_DELETE_MINS = int(
+    getenv("CLEANMODE_MINS", "5")
+)  # Remember to give value in Seconds
+
+# Get your pyrogram v2 session from @Champu_STRING_ROBOT on Telegram
+STRING1 = getenv("STRING_SESSION", None)
+STRING2 = getenv("STRING_SESSION2", None)
+STRING3 = getenv("STRING_SESSION3", None)
+STRING4 = getenv("STRING_SESSION4", None)
+STRING5 = getenv("STRING_SESSION5", None)
+
+BANNED_USERS = filters.user()
+adminlist = {}
+lyrical = {}
+votemode = {}
+autoclean = []
+confirmer = {}
+chatstats = {}
+userstats = {}
+clean = {}
+
+autoclean = []
+
+START_IMG_URL = getenv(
+    "START_IMG_URL", "https://telegra.ph/file/3c3ab79fba0fa91a4ae8e.jpg"
+)
+PING_IMG_URL = getenv(
+    "PING_IMG_URL", "https://telegra.ph/file/c603be89cbd04d9d49ebd.jpg"
+)
+PLAYLIST_IMG_URL = "https://telegra.ph/file/b838a5d402196d30068ce.jpg"
+STATS_IMG_URL = "https://telegra.ph/file/c71f53a6d7ac49f57e9b8.jpg"
+TELEGRAM_AUDIO_URL = "https://telegra.ph/file/b838a5d402196d30068ce.jpg"
+TELEGRAM_VIDEO_URL = "https://telegra.ph/file/b838a5d402196d30068ce.jpg"
+STREAM_IMG_URL = "https://telegra.ph/file/bd24003547eb253e2bb9a.jpg"
+SOUNCLOUD_IMG_URL = "https://telegra.ph/file/8d050586cd6693e8f2ee6.jpg"
+YOUTUBE_IMG_URL = "https://telegra.ph/file/8d050586cd6693e8f2ee6.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://telegra.ph/file/c71f53a6d7ac49f57e9b8.jpg"
+SPOTIFY_ALBUM_IMG_URL = "https://telegra.ph/file/c71f53a6d7ac49f57e9b8.jpg"
+SPOTIFY_PLAYLIST_IMG_URL = "https://telegra.ph/file/c71f53a6d7ac49f57e9b8.jpg"
+
+
+def time_to_seconds(time):
+    stringt = str(time)
+    return sum(int(x) * 60**i for i, x in enumerate(reversed(stringt.split(":"))))
+
+
+DURATION_LIMIT = int(time_to_seconds(f"{DURATION_LIMIT_MIN}:00"))
+
+
+if SUPPORT_CHANNEL:
+    if not re.match("(?:http|https)://", SUPPORT_CHANNEL): "https://t.me/botkild"
+        raise SystemExit(
+            "[ERROR] - Your SUPPORT_CHANNEL url is wrong. Please ensure that it starts with https://"
+        )
+
+if SUPPORT_CHAT:
+    if not re.match("(?:http|https)://", SUPPORT_CHAT): "https://t.me/botkilda"
+        raise SystemExit(
+            "[ERROR] - Your SUPPORT_CHAT url is wrong. Please ensure that it starts with https://"
+        )
