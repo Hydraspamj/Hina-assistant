8afa90efd42ed432f748ebbc1d33066dd3b4515a
Sinuhan <167394965+Hydraspamj@users.noreply.github.com>
2025-10-17 02:53:11 +0530

    Update __init__.py

--- strings/__init__.py
+++ strings/__init__.py
@@ -1,3 +1,7 @@
 def get_command(*args, **kwargs):
     # Dummy placeholder to avoid ImportError
     return None
+
+def get_string(*args, **kwargs):
+    # Dummy placeholder to avoid ImportError
+    return None
